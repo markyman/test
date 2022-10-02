68f005be5a3e3367ac4f3b8ce2e7050759f23989
D3ATHBRINGER13 <53559772+D3ATHBRINGER13@users.noreply.github.com>
2022-10-02 13:24:23 +0100

    try calling fileid directly

--- get_command_param_table.js
+++ get_command_param_table.js
@@ -11,26 +11,26 @@ function readStdString(ptr) {
     return ptr.add(2 * Process.pointerSize).readUtf8String();
 }
 
-let interceptor = Interceptor.attach(ptr_command_registry__is_valid, {
-    onEnter: function (args) {
-        this.ptr = args[0];
-    },
-    onLeave: function (retval) {
-        interceptor.detach();
-        let fn_command_registry__is_valid = new NativeFunction(ptr_command_registry__is_valid, 'bool', ['pointer', 'pointer']);
-        let fn_command_registry__symbol_to_string = new NativeFunction(ptr_command_registry__symbol_to_string, 'pointer', ['pointer', 'pointer', 'pointer'])
-        let ptr_symbol = Memory.alloc(4);
-        let ptr_string = Memory.alloc(64);
-
-
-        for (let i = 0; i < 1000; i++) {
-            ptr_symbol.writeInt(i | 0x100000);
-            console.log(i, fn_command_registry__is_valid(this.ptr, ptr_symbol));
-            console.log(hexdump(fn_command_registry__symbol_to_string(this.ptr, ptr_symbol, ptr_string)));
-            console.log(hexdump(ptr_string));
-        }
-    }
-});
+// let interceptor = Interceptor.attach(ptr_command_registry__is_valid, {
+//     onEnter: function (args) {
+//         this.ptr = args[0];
+//     },
+//     onLeave: function (retval) {
+//         interceptor.detach();
+//         let fn_command_registry__is_valid = new NativeFunction(ptr_command_registry__is_valid, 'bool', ['pointer', 'pointer']);
+//         let fn_command_registry__symbol_to_string = new NativeFunction(ptr_command_registry__symbol_to_string, 'pointer', ['pointer', 'pointer', 'pointer'])
+//         let ptr_symbol = Memory.alloc(4);
+//         let ptr_string = Memory.alloc(64);
+//
+//
+//         for (let i = 0; i < 1000; i++) {
+//             ptr_symbol.writeInt(i | 0x100000);
+//             console.log(i, fn_command_registry__is_valid(this.ptr, ptr_symbol));
+//             console.log(hexdump(fn_command_registry__symbol_to_string(this.ptr, ptr_symbol, ptr_string)));
+//             console.log(hexdump(ptr_string));
+//         }
+//     }
+// });
 
 // let interceptor = Interceptor.attach(ptr_google_breakpad__fileid__fileid, {
 //     onEnter: function (args) {
@@ -44,4 +44,7 @@ let interceptor = Interceptor.attach(ptr_command_registry__is_valid, {
 //     },
 // });
 
-console.log("Hooked into process. Please start a new single player game.");
+let fn_google_breakpad__fileid__fileid = new NativeFunction(ptr_google_breakpad__fileid__fileid, 'pointer', ['pointer', 'pointer']);
+console.log(hexdump(fn_google_breakpad__fileid__fileid(Memory.alloc(64), Module.getBaseAddress("libminecraftpe.so").add(0x0C5E3438))));
+
+// console.log("Hooked into process. Please start a new single player game.");
